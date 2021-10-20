27a3415b54888dae175f9e01ef7fbd6b0f7e07c9
Mayank Ramani <mayankramani989@gmail.com>
2021-10-20 22:51:28 +0530

    mayank's commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './user/user-dashboard/user-dashboard.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends  AdminDashboardComponent{
   title = 'gladiator';
+
+
+
+   success():boolean{
+    let logininfo=sessionStorage.getItem("isLoggedinAdmin");
+    if(logininfo=='true'){
+   
+    return true;
+    }
+    else
+      {
+        return false;
+      }
+  }
+
 }
